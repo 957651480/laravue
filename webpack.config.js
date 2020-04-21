3ad64b0a967c98c43c429dc3d4c054be59f8b342
yangtaihua <957651480@qq.com>
2020-04-21 16:15:31 +0800

    分离element -ui和echarts

--- webpack.config.js
+++ webpack.config.js
@@ -39,5 +39,22 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        elementUI: {
+          name: 'js/chunk-elementUI', // split elementUI into a single package
+          priority: 100, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+          test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
+        },
+        echarts: {
+          name: 'js/chunk-echarts', // split elementUI into a single package
+          priority: 90, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+          test: /[\\/]node_modules[\\/]_?echarts(.*)/, // in order to adapt to cnpm
+        },
+      },
+    },
+  },
   plugins: plugins,
 };
