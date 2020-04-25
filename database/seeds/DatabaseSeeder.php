a04e8a593bacb3d4613b97fb989707f708ffb499
yangtaihua <957651480@qq.com>
2020-04-25 23:52:58 +0800

    修改

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => Hash::make('admin'),
         ]);
-        $manager = User::create([
+       /* $manager = User::create([
             'name' => 'manager',
             'email' => 'manager@manager.com',
             'password' => Hash::make('admin'),
@@ -38,18 +38,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'visitor',
             'email' => 'visitor@visitor.com',
             'password' => Hash::make('admin'),
-        ]);
+        ]);*/
 
         $adminRole = Role::findByName(\App\Laravue\Acl::ROLE_ADMIN);
-        $managerRole = Role::findByName(\App\Laravue\Acl::ROLE_MANAGER);
+       /* $managerRole = Role::findByName(\App\Laravue\Acl::ROLE_MANAGER);
         $editorRole = Role::findByName(\App\Laravue\Acl::ROLE_EDITOR);
         $userRole = Role::findByName(\App\Laravue\Acl::ROLE_USER);
-        $visitorRole = Role::findByName(\App\Laravue\Acl::ROLE_VISITOR);
+        $visitorRole = Role::findByName(\App\Laravue\Acl::ROLE_VISITOR);*/
         $admin->syncRoles($adminRole);
-        $manager->syncRoles($managerRole);
+       /* $manager->syncRoles($managerRole);
         $editor->syncRoles($editorRole);
         $user->syncRoles($userRole);
-        $visitor->syncRoles($visitorRole);
+        $visitor->syncRoles($visitorRole);*/
         $this->call(UsersTableSeeder::class);
     }
 }
