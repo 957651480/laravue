346fb6622afd2fc71f19952d21b7d7b59d68fe14
yangtaihua <957651480@qq.com>
2020-04-26 10:06:31 +0800

    appId修改

--- config/wechat.php
+++ config/wechat.php
@@ -1,6 +1,6 @@
 <?php
 return [
 
-    'app_id' => env('APP_ID','wxe02410441ea47ba5'),
-    'secret' => env('APP_SECRET','d730d25795ccf699d3876d7fd4f9fc85'),
+    'app_id' => env('APP_ID','wx03837cfa1f460081'),
+    'secret' => env('APP_SECRET','b677160fc4886bf46a223e0486dbc958'),
 ];
