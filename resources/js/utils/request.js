0d85e09329e4de003704807eaa8e182b5cf98256
yangtaihua <957651480@qq.com>
2020-05-01 00:22:50 +0800

    修改

--- resources/js/utils/request.js
+++ resources/js/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Message } from 'element-ui';
 import { getToken, setToken } from '@/utils/auth';
+import store from "@/store";
 
 // Create axios instance
 const service = axios.create({
@@ -28,27 +29,44 @@ service.interceptors.request.use(
 // response pre-processing
 service.interceptors.response.use(
   response => {
-    if (response.headers.authorization) {
-      setToken(response.headers.authorization);
-      response.data.token = response.headers.authorization;
-    }
+    if (response.status !== 200) {
+      Message({
+        message: response.msg || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      });
 
-    return response.data;
+      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+      /*if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // to re-login
+        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+          confirmButtonText: 'Re-Login',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }*/
+      return Promise.reject(new Error(response.msg || 'Error'))
+    } else {
+
+      if (response.headers.authorization) {
+        setToken(response.headers.authorization);
+        response.data.token = response.headers.authorization;
+      }
+      return response.data;
+    }
   },
   error => {
-    let message = error.message;
-    if (error.response.data && error.response.data.errors) {
-      message = error.response.data.errors;
-    } else if (error.response.data && error.response.data.error) {
-      message = error.response.data.error;
-    }
-
+    /*let message = error.msg;
     Message({
       message: message,
       type: 'error',
       duration: 5 * 1000,
     });
-    return Promise.reject(error);
+    return Promise.reject(error);*/
   },
 );
 
